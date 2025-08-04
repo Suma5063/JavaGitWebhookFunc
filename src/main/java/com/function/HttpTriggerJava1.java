33ce08f33bf12119a58b33e16247efb80cfdec41
Suma <sumah8050@gmail.com>
2025-08-04 17:21:25 +0530

    Initial commit with Azure Function

--- src/main/java/com/function/HttpTriggerJava1.java
+++ src/main/java/com/function/HttpTriggerJava1.java
@@ -21,7 +21,9 @@ public class HttpTriggerJava1 {
 
         // Parse query parameter
         String query = request.getQueryParameters().get("name");
-        String name = request.getBody().orElse(query);
+            context.getLogger().info("Query param: " + query);
+
+        String name = request.getBody().orElse(query);        
 
         if (name == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
